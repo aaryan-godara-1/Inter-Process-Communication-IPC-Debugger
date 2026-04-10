3f82257f17f3eab31ba1579decaceb267f6a8ce7
Swarnim Singh <swarnimsingh@Sankalps-MacBook-Pro.local>
2026-04-10 12:18:04 +0530

    Implemented deadlock detection using DFS

--- ipc_debugger/analytics/analysis.py
+++ ipc_debugger/analytics/analysis.py
@@ -5,12 +5,12 @@ def read_logs():
     print("Reading logs...")
     return [("P1", "P2"), ("P2", "P3"), ("P3", "P1")]
 
-def main():
-    data = read_logs()
-    print("Logs:", data)
+# def main():
+#     data = read_logs()
+#     print("Logs:", data)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 def build_graph(logs): #takes input like [("P1", "P2"), ("P2", "P3"), ("P3", "P1")]
     graph={} # we are creating empty graph over here
     for a, b in logs: #looping through logs
@@ -18,3 +18,48 @@ def build_graph(logs): #takes input like [("P1", "P2"), ("P2", "P3"), ("P3", "P1
             graph[a] = []
         graph[a].append(b)
     return graph
+
+#Deadlock work 
+def detect_deadlock(graph):
+    visited = set() #stores node we already visited, avoids repeating work
+    stack = set() #tracks current path
+
+    def dfs(node):
+        if node in stack:
+            return True
+        
+        if node in visited:
+            return False
+        
+        visited.add(node)
+        stack.add(node)
+
+        for neighbor in graph.get(node, []):
+            if dfs(neighbor):
+                return True
+        
+        stack.remove(node)
+        return False
+
+    for node in graph:
+        if dfs(node):
+            return True
+
+    return False
+
+    #Main function 
+def main():
+    logs = read_logs()
+    graph = build_graph(logs)
+
+    print("Graph:", graph)
+
+    if detect_deadlock(graph):
+        print("Deadlock detected 💀")
+    else:
+        print("No deadlock ✅")
+
+
+# Run program
+if __name__ == "__main__":
+    main()
